1143541dc781a436f405d2d601619663d84daedc
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-07-16 10:24:40 +0500

    use registration func. done

---
+++ backend/models/userModels.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
+
+const UserSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+
+    }
+)
+
+// 'pre' is a Mongoose middleware function that allows code to run before an event ('save' event here)
+
+UserSchema.pre('save', async function (next) {
+    // Checks if the password field has not been modified and skips the rest of the middleware if it hasn't
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    // But, If the password field has been modified (e.g., user has set a new password), hash it before saving to the database
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password = await bcrypt.hash(this.password, salt)
+
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export default User 
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,39 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
 dotenv.config();
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+// Import custom middleware functions for error handling
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 import connectDB from "./config/db.js";
+
+// Define the port to run the server, falling back to 5000 if not specified in environment variables
 const port = process.env.PORT || 5000;
 
 import userRoutes from "./routes/userRoutes.js";
 
+// Establish a connection to the database
 connectDB();
 
+// Initialize the Express application
 const app = express();
 
-app.use('/api/users', userRoutes);
+// Middleware to parse incoming JSON payloads
+app.use(express.json());
+// Middleware to parse incoming URL-encoded data(form data)
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send(`Server is ready!`))
+// Use the user routes for any requests to /api/users
+app.use("/api/users", userRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+// Define a simple route for the root URL
+app.get("/", (req, res) => res.send(`Server is ready!`));
 
-app.listen(port, () => console.log(`Server started on  PORT : ${port}`))
+// Middleware for handling 404 errors (not found)
+app.use(notFound);
+// Middleware for handling errors
+app.use(errorHandler);
+
+// Start the server and listen on the specified port
+app.listen(port, () => console.log(`Server started on  PORT : ${port}`));
