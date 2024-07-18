976ca447bde8120c9068bc02bcdfa9229ae0005c
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-07-18 17:39:57 +0500

    add updateProfile func.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,7 +72,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 
     const user = {
-        id: req.user._id,
+        _id: req.user._id,
         name: req.user.name,
         email: req.user.email,
     }
@@ -84,7 +84,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //  @access   Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Update User Profile" });
+
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res
+            .status(200)
+            .json({
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+            })
+    } else {
+        res.status(303);
+        throw new Error('User not found!')
+    }
+
 });
 
 //  @desc     Logout a user
