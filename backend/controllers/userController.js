afc922fb2f6d2c696ecbc17fb0715a1822315249
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-07-18 05:59:47 +0500

    logout user & destory cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,7 +85,17 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 //  @access   Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Logout User" });
+
+    res.cookie(
+        'jwt',
+        '',
+        {
+            httpOnly: true,
+            expires: new Date(0)
+        }
+    );
+
+    res.status(200).json({ message: "User logged out" });
 });
 
 export {
