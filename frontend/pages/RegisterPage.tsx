2543a7ef2e712a0b9d9cf5e64e893e17762a0ee7
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-07-22 09:20:07 +0500

    feat(register): connect RTK to registration page and enable user registration

--- frontend/pages/RegisterPage.tsx
+++ frontend/pages/RegisterPage.tsx
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
-import FormContainer from '../components/FormContainer';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import { useRegisterMutation } from "../src/slices/usersApiSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
+import { setCredentials } from "../src/slices/authSlice";
 
 const RegisterPage: React.FC = () => {
-    const [name, setName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [confirmPassword, setConfirmPassword] = useState<string>('');
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [confirmPassword, setConfirmPassword] = useState<string>("");
 
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+    const { userInfo } = useSelector((state: any) => state.auth);
+
+    const [register, { isLoading }] = useRegisterMutation();
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate("/")
+        }
+    }, [navigate, userInfo])
     const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('Name:', name);
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('Confirm Password:', confirmPassword);
+        if (password !== confirmPassword) {
+            toast.error("Passwords do not match!")
+        } else {
+            try {
+                const res = await register({ name, email, password }).unwrap();
+                dispatch(setCredentials({ ...res }));
+                navigate('/')
+            } catch (err: any) {
+                toast.error(err?.data?.message || err.error)
+            }
+        }
     };
 
     return (
@@ -22,7 +46,7 @@ const RegisterPage: React.FC = () => {
             <h1>Register</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId="name">
-                    <Form.Label className='mt-3'>Name</Form.Label>
+                    <Form.Label className="mt-3">Name</Form.Label>
                     <Form.Control
                         type="text"
                         placeholder="Enter name"
@@ -32,7 +56,7 @@ const RegisterPage: React.FC = () => {
                 </Form.Group>
 
                 <Form.Group controlId="email">
-                    <Form.Label className='mt-3'>Email Address</Form.Label>
+                    <Form.Label className="mt-3">Email Address</Form.Label>
                     <Form.Control
                         type="email"
                         placeholder="Enter email"
@@ -42,7 +66,7 @@ const RegisterPage: React.FC = () => {
                 </Form.Group>
 
                 <Form.Group controlId="password">
-                    <Form.Label className='mt-3'>Password</Form.Label>
+                    <Form.Label className="mt-3">Password</Form.Label>
                     <Form.Control
                         type="password"
                         placeholder="Enter password"
@@ -52,7 +76,7 @@ const RegisterPage: React.FC = () => {
                 </Form.Group>
 
                 <Form.Group controlId="confirmPassword">
-                    <Form.Label className='mt-3'>Confirm Password</Form.Label>
+                    <Form.Label className="mt-3">Confirm Password</Form.Label>
                     <Form.Control
                         type="password"
                         placeholder="Confirm password"
@@ -61,7 +85,12 @@ const RegisterPage: React.FC = () => {
                     />
                 </Form.Group>
 
-                <Button type="submit" variant="primary" className="mt-3">
+                {isLoading && <Loader />}
+                <Button
+                    type="submit"
+                    variant="primary"
+                    className="mt-3"
+                >
                     Register
                 </Button>
             </Form>
@@ -69,9 +98,7 @@ const RegisterPage: React.FC = () => {
             <Row className="py-3">
                 <Col>
                     Already have an Account?
-                    <Link to="/login">
-                        Login
-                    </Link>
+                    <Link to="/login">Login</Link>
                 </Col>
             </Row>
         </FormContainer>
