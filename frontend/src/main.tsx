98b8ff7cee6a30b48bb29e8d8d6ff0bc6db535aa
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-07-22 09:42:33 +0500

    Add PrivateRoutes component for protected route handling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,8 +12,10 @@ import {
 import HomePage from "../pages/HomePage.tsx";
 import LoginPage from "../pages/LoginPage.tsx";
 import RegisterPage from "../pages/RegisterPage.tsx";
+import ProfilePage from "../pages/ProfilePage.tsx";
 import store from './store.ts'
 import { Provider } from "react-redux";
+import PrivateRoutes from "../components/PrivateRoute.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +23,10 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      {/* Private Routes */}
+      <Route path="" element={<PrivateRoutes />}>
+        <Route path="/profile" element={<ProfilePage />} />
+      </Route>
     </Route>
   )
 )
