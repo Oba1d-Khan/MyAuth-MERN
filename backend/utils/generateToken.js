cc59cecd1a330022ceeaa042910ef91f1a33274a
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2025-07-08 17:05:32 +0500

    changes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,23 +1,22 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
+  const token = jwt.sign(
+    { userId }, // payload
+    process.env.JWT_SECRET, // secret key
+    { expiresIn: "30d" } // expire option
+  );
 
-    const token = jwt.sign(
-        { userId },     // payload
-        process.env.JWT_SECRET,     // secret key
-        { expiresIn: '30d' }    // expire option
-    );
-
-    res.cookie(
-        'jwt',  // name of cookie
-        token,  // value of cookie to be stored
-        {
-            httpOnly: true,  // Secure from XSS (JavaScript) attacks
-            secure: process.env.NODE_ENV !== 'development', // Ensure cookies are only sent over HTTPS in production
-            sameSite: 'strict', // Helps prevent CSRF attacks
-            maxAge: 30 * 24 * 60 * 60 * 1000 // Expiry time set to 30 days (in milliseconds)
-        }
-    );
-}
+  res.cookie(
+    "jwt", // name of cookie
+    token, // value of cookie to be stored
+    {
+      httpOnly: true, // Secure from XSS (JavaScript) attacks
+      secure: process.env.NODE_ENV !== "development", // Ensure cookies are only sent over HTTPS in production
+      sameSite: "none", // Helps prevent CSRF attacks
+      maxAge: 30 * 24 * 60 * 60 * 1000, // Expiry time set to 30 days (in milliseconds)
+    }
+  );
+};
 
 export default generateToken;
